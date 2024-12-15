128fcf1b807e41bec8cb9e2ac651d4dc45114eca
Zakria-04 <zakriaabdelgny04@gmail.com>
2024-12-15 23:14:23 +0200

    db connection

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,32 @@
 import express from "express";
 import cors from "cors";
+import mongoose from "mongoose";
+import * as dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+const dbURL = process.env.DB_URL;
+
+//TODO => if you find this error you need to create .env file and add your mongoDB url inside DB_URL variable
+if (!dbURL) {
+  throw new Error("Missing DB_URL environment variable");
+}
+
+mongoose.connect(dbURL);
+
+mongoose.connection.on("connected", () => {
+  console.log("mongoose connected successfully!");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error("mongoose connection error", err);
+});
+
+app.get("/live", (req, res) => {
+  res.status(200).json({ status: "server is live!" });
+});
 
 export default app;
